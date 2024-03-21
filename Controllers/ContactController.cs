430b886bee8c9985f64e9909f75bc199c45b90db
Danilo Mendonça <danilomendonca08@gmail.com>
2024-03-21 20:09:23 -0300

    add Contact e About views

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Checkpoint1.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
